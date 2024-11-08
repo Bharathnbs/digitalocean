7ceccf673986cccfdb5fcaaeeeee6c7a3c4eb649
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-11-08 14:39:45 +0530

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.site')
+
+@section('content')
+<div>
+    <div>I'm bharath Fullstack Developer</div>
+</div>
+@endsection
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('content')
+    <div>
+        <div>Hi Welcome to new learning zone</div>
+        <a href="{{route('about')}}">about</a>
+    </div>
+@endsection
+
