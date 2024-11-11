14e6ebfa338d92a587f6bd336e9e83cc3cda87db
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-11-11 15:00:42 +0530

    landing page  - add some test

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div>
         <div>Hi Welcome to new learning zone</div>
+        <div>Hi Welcome Tamil</div>
+        <div>Hi Welcome Shivaram</div>
+
         <a href="{{route('about')}}">about</a>
     </div>
 @endsection
