00d74fbaaef469c14714611e683545fb9407c6b9
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-11-12 20:55:19 +0530

    about - bug fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div>
     <div>I'm bharath Fullstack Developer</div>
-    <a href="{{route('landing')}}">Home</a>
+    <a href="{{route('home')}}">Home</a>
 </div>
 @endsection
